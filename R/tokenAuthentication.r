7a0215f5ee2ba9c40543f7bef61aea5a0684dbb4
MarkoKazimirovic <marko.kazimirovic@sfb.bg.ac.rs>
2022-10-11 09:55:44 +0200

    authentication

--- R/tokenAuthentication.r
+++ R/tokenAuthentication.r
@@ -1,20 +1,49 @@
+#'Token authentication
+#'
+#'This function checks if the given credentials are entered properly to enable the connection to the JSON file.
+#'Submitting a non-existing user-name or token will result in an unauthorized access error.
+#'
+#' @param userName Must be entered as a single string without whitespace, the violation will throw a dedicated error.
+#' @param token Must be entered as a single string without whitespace, the violation will throw a dedicated error.
+#'
+#' @return tokenAuthentication() for authenticated user return a list of credentials (user-name, token, url) along with httr authentication() and response() objects
+#' @export
+#'
+#' @examples
+#' ## Wrong entered credintials
+#' tokenAuthentication("Mickey mouse", ".")
+
+
+
+tokenAuthentication <- function(userName = NULL, token= NULL){
+  cred <- textCheck(userName, token)
+  userAuth <- authenticate(cred$user,
+                           cred$pass)
+  response <- GET(url = cred$url
+                  , config = userAuth
+                  )
+  statusCheck(response)
+  c(cred,
+    userAuth = list(userAuth),
+    response = list(response))
+}
+
 textCheck <- function(userName, token){
-  if(!is.character(userName) || !length(userName) == 1 || grepl("\\W", userName)){
+  if(!is.character(userName) || length(userName) != 1 || grepl("\\W", userName)){
     stop("User name is not entered properly!", call. = FALSE)
   }
-  if(!is.character(token) || !length(token) == 1 || grepl("\\W", token)){
+  if(!is.character(token) || length(token) != 1 || grepl("\\W", token)){
     stop("Token is not entered properly!", call. = FALSE)
   }
   list(user  = userName,
        pass  = token,
-       url = modify_url("https://api.github.com/",
-                        path = paste0("user/repos"))
+       url   = "https://api.github.com/user/repos"
   )
 }
 
 statusCheck <- function(APIresponse){
   if(APIresponse$status_code == 401 || length(content(APIresponse)) == 0) {
-    stop("Unauthorized acces with invalid user name or password", call. = FALSE)
+    stop("Unauthorized access with invalid user name or password", call. = FALSE)
   } else if (APIresponse$status_code != 200){
     stop("Something went wrong", call. = FALSE)
   } else if (http_type(APIresponse) != "application/json") {
@@ -22,18 +51,6 @@ statusCheck <- function(APIresponse){
   }
 }
 
-tokenAuthentication <- function(userName, token){
-  cred <- textCheck(userName, token)
-  userAuth <- authenticate(cred$user,
-                           cred$pass)
-  response <- GET(url = cred$url
-                  , config = userAuth
-                  # , query = list(visibility= "all")
-  )
-  statusCheck(response)
-  c(cred,
-    userAuth = list(userAuth),
-    response = list(response))
-}
+
 
 
